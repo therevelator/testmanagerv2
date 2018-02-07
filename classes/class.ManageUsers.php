92f875213d17e85561bbda15002ada6389b95982
therevelator <ionut.n.apostu@gmail.com>
2018-02-07 23:06:02 +0000

    added granular classes, dbConnect and ManageUsers

---
+++ classes/class.ManageUsers.php
@@ -0,0 +1,52 @@
+<?php
+  include_once('class.dbconnect.php');
+
+  /**ManageUsers: allows the users to be registered and logged in
+   *
+   */
+  class ManageUsers
+  {
+    public $link;
+
+    function __construct() {
+      $db_connect = new dbConnect();
+      $this->link = $db_connect->connect();
+      return $this->link;
+    }
+
+    function registerUsers ($username, $password, $ip_address, $reg_time, $reg_date, $email) {
+      //create prepared statement using PDO
+      $query = $this->link->prepare("use testmanager");
+      $query->execute();
+      $query = $this->link->prepare("INSERT INTO users (username, password, ip_address, reg_time, reg_date, email) VALUES (?,?,?,?,?,?)");
+      //the question marks in the VALUES paranthesis are for protecting the DB against injection
+      $values = array($username, $password, $ip_address, $reg_time, $reg_date, $email);
+      //executes the query prepared earlier
+      $query->execute($values);
+      //counts number of rows inserted
+      $counts = $query->rowCount();
+      return $counts;
+
+    }
+}
+
+$users = new ManageUsers();
+echo $users->registerUsers('ionut', 'pass', '127.127.127.127', '12:00:05', '07-02-2018', 'ionut.apostu@mail.com');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ classes/class.dbconnect.php
@@ -0,0 +1,49 @@
+<?php
+    /**This is the class with which we connect to the DB
+     * MySQL, hosted locally
+     */
+    class dbConnect
+    {
+      protected $db_conn;
+      public $db_name = "testmanager";
+      public $db_user = "root";
+      public $db_pass = "";
+      public $db_host = "localhost";
+
+      function connect() {
+        try {
+          $this->db_conn = new PDO ("mysql:host=localhost;db_name=testmanager", $this->db_user, $this->db_pass);
+          $this->db_conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+          $this->db_conn->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, true);
+          return $this->db_conn;
+        } catch (Exception $e) {
+          var_dump ($e->getMessage());
+    }
+
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
