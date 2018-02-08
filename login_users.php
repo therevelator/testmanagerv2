63e7117078e2adf690ce8bf0b002f9d4c1e9928f
therevelator <ionut.n.apostu@gmail.com>
2018-02-08 16:07:28 +0000

    updated comments

--- login_users.php
+++ login_users.php
@@ -1,17 +1,14 @@
 <?php
 
 include_once('classes/class.ManageUsers.php');
-//include_once('../classes/class.errors.php');
 /**
- *
+ *this class checks if everything is ok with the form and validates stuff
  */
 class reg
 {
 
   function __construct()
   {
-
-
       $users = new ManageUsers();
       $username = $_POST['username'];
       $password = $_POST['password'];
@@ -20,12 +17,16 @@ class reg
       $ip_address = $_SERVER['REMOTE_ADDR'];
       $reg_date = date("Y-m-d");
       $reg_time = date("H:i:s");
-
       if (empty($username) || empty($password) || empty($email) || empty($repassword) ) {
-        //maybe validate user input with a class and functions, might work better
         return $_SESSION['error'] = "<script type=\"text/javascript\">swal(\"Nope!\", \"All fields are required\", \"error\");</script>";
         header ('Location: register.php');
         //add an elseif and check $password !== $repassword, and then give the error
+      }elseif ($password !== $repassword) {
+        return $_SESSION['error'] = "<script type=\"text/javascript\">swal(\"Nope!\", \"Passwords must match\", \"error\");</script>";
+        header ('Location: register.php');
+      } elseif (strlen($password) <= 7) {
+        return $_SESSION['error'] = "<script type=\"text/javascript\">swal(\"Nope!\", \"Password is too short. Try more characters\", \"error\");</script>";
+        header ('Location: register.php');
       } else {
         $check_availability = $users->GetUserInfo($username);
         if ($check_availability == 0) {
@@ -34,15 +35,10 @@ class reg
             $_SESSION['error'] = NULL;
             $make_sessions = $users->GetUserInfo($username);
             print_r($make_sessions);
-          }
-        } else {
-          
-          // header ('Location: register.php');
         }
       }
-
     }
-
+  }
 }
 
 
--- register.php
+++ register.php
@@ -10,48 +10,46 @@
   <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
   <link type="text/css" rel="stylesheet" href="css/sweetalert.css" />
   <script src="js/sweetalert.min.js"></script>
-
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 </head>
 <body>
   <!-- DC iMenu Start -->
-
+<!-- look for a nice menu -->
 <!-- DC iMenu End -->
 <div align="center">
   <div class="wrapper" >
     <h1>Welcome to the Test Manager V2</h1>
     <p>To sign-up for an account please fill in the form below</p>
     <form class="form" method="post" action="">
-
       <input type="text" class="username" name="username" placeholder="Username">
       <div>
-        <!-- check errors / validators -->
       </div>
       <input type="email" class="email" name="email" placeholder="Email">
        <div>
-
       </div>
       <input type="password" class="password" name="password" placeholder="Password">
       <div>
-
       </div>
       <input type="password" class="password" name="repassword" placeholder="Retype Password">
       <div>
-
       </div>
       <input type="submit" class="submit" name="register" value="Register">
     </form>
-    <?php
+<?php
+//check if the button is pressed
 if (isset($_POST['register'])) {
   include_once('login_users.php');
+  //instantiate the register users class
   $user = new reg();
 }
+//checks if any errors have been stored in the session
 if (isset($_SESSION['error'])) {
+  //and echoes if not empty
   echo $_SESSION['error'];
 }
+//after done displaying errors, unset errors variable.
 $_SESSION['error'] = NULL;
-
- ?>
+?>
   </div>
 </div>
 
