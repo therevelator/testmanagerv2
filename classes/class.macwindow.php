0b4f5140f5a01f60b008008c23b5e46ba81c800a
therevelator <ionut.n.apostu@gmail.com>
2018-02-09 01:41:45 +0000

    added new styles and new dashboard

---
+++ classes/class.macwindow.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ *
+ */
+class MacWindow
+{
+  //public $welcome = NULL;
+  function __construct()
+  {
+    echo '<div class="window">
+  <div class="titlebar">
+    <div class="buttons">
+      <div class="close">
+        <a class="closebutton" href="#"><span><strong>x</strong></span></a>
+        <!-- close button link -->
+      </div>
+      <div class="minimize">
+        <a class="minimizebutton" href="#"><span><strong>&ndash;</strong></span></a>
+        <!-- minimize button link -->
+      </div>
+      <div class="zoom">
+        <a class="zoombutton" href="#"><span><strong>+</strong></span></a>
+        <!-- zoom button link -->
+      </div>
+    </div>
+    TITLE can be DYNAMIC
+    <!-- window title -->
+  </div>
+  <div class="content">
+  </div>
+</div>';
+}
+}
+
+ ?>
--- dashboard.php
+++ dashboard.php
@@ -1,11 +1,9 @@
-<?php
-//manage the headers somehow, include dependencies
-//make a navbar, multiple choices available
-//make a table with initial selections or a menu with redirecting to docs,
-//include and fix that MacOS first page, maybe
-//projects (subsection is testcase or section / subsection)
-//implement errors and notifications
-//implement user messages through session variables or something else maybe stored in the db and once read, set a flag to read or something
-//implement alerts, like meeting alerts, with JS maybe, if PHP not possible (or even AJAX);
-//search function with AJAX (very important)
-?>
+<?php include_once('classes/class.header.php'); $headers = new headers(); ?>
+<link type="text/css" rel="stylesheet" href="css/navbar.css" />
+<link type="text/css" rel="stylesheet" href="css/macwindow.css" />
+
+<body>
+<!-- create a class which exports html with parameters in the mac window-->
+<?php include_once('classes/class.macwindow.php');
+$macWindow = new MacWindow();//add parameters?>
+</body>
