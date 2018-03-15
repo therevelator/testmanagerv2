20fe36081ebee0cbc49a4f0d406c0449f01869f2
therevelator <ionut.n.apostu@gmail.com>
2018-03-15 13:54:04 +0000

    Created logic for table info to be fetched from the DB automatically (have to improve getting headers, currently hard-coded)

--- classes/class.table.php
+++ classes/class.table.php
@@ -1,5 +1,6 @@
 <?php
-
+include_once("class.dbconnect.php");
+$connection = new dbConnect();
 /**
  * This class loads the table template
  */
@@ -15,80 +16,26 @@ class table
 				<th>ID</th>
 				<th>Name</th>
 				<th>Created By</th>
-				<th>Actions</th>
+				<th>Description</th>
+        <th>Actions</th>
 			</tr>
 		</thead>
-		<tbody>
-			<tr>
-				<td>Cell 1</td>
-				<td>Cell 2</td>
-				<td>Cell 3</td>
-				<td><button class="btndelete">Delete</button>
-        <button class="btndetails">Details</button></td>
-			</tr>
-      <tr>
-				<td>Cell 1</td>
-				<td>Cell 2</td>
-				<td>Cell 3</td>
-				<td><button class="btndelete">Delete</button>
-        <button class="btndetails">Details</button></td>
-			</tr>
-      <tr>
-				<td>Cell 1</td>
-				<td>Cell 2</td>
-				<td>Cell 3</td>
-				<td><button class="btndelete">Delete</button>
-        <button class="btndetails">Details</button></td>
-			</tr>
-      <tr>
-				<td>Cell 1</td>
-				<td>Cell 2</td>
-				<td>Cell 3</td>
-				<td><button class="btndelete">Delete</button>
-        <button class="btndetails">Details</button></td>
-			</tr>
-      <tr>
-				<td>Cell 1</td>
-				<td>Cell 2</td>
-				<td>Cell 3</td>
-				<td><button class="btndelete">Delete</button>
-        <button class="btndetails">Details</button></td>
-			</tr>
-      <tr>
-				<td>Cell 1</td>
-				<td>Cell 2</td>
-				<td>Cell 3</td>
-				<td><button class="btndelete">Delete</button>
-        <button class="btndetails">Details</button></td>
-			</tr>
-      <tr>
-				<td>Cell 1</td>
-				<td>Cell 2</td>
-				<td>Cell 3</td>
-				<td><button class="btndelete">Delete</button>
-        <button class="btndetails">Details</button></td>
-			</tr>
-      <tr>
-				<td>Cell 1</td>
-				<td>Cell 2</td>
-				<td>Cell 3</td>
-				<td><button class="btndelete">Delete</button>
-        <button class="btndetails">Details</button></td>
-			</tr>
-      <tr>
-				<td>Cell 1</td>
-				<td>Cell 2</td>
-				<td>Cell 3</td>
-				<td><button class="btndelete">Delete</button>
-        <button class="btndetails">Details</button></td>
-			</tr>
-      <tr>
-				<td>Cell 1</td>
-				<td>Cell 2</td>
-				<td>Cell 3</td>
-				<td><button class="btndelete">Delete</button>
-        <button class="btndetails">Details</button></td>
-			</tr>
+		<tbody>';
+    $link = mysqli_connect("127.0.0.1", "root", "", "testmanager");
+    $sql="SELECT ID, ProjectName, CreatedBy, Description FROM projects ORDER BY ID";
+    $result=mysqli_query($link,$sql);
+      while ($row = mysqli_fetch_assoc($result)) {
+      $ID = $row['ID'];
+        echo '<tr>
+  				<td>'; echo $row['ID']; echo '</td>
+  				<td>'; echo $row['ProjectName']; echo '</td>
+  				<td>'; echo $row['CreatedBy']; echo '</td>
+          <td>'; echo $row['Description']; echo '</td>
+  				<td><button class="btndelete">Delete</button>
+          <button class="btndetails">Details</button></td>
+  			</tr>';
+      }
+    echo '
 		</tbody>
 	</table>
 </div>';
